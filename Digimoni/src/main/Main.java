81006337ea35a90c0296b5deac0bc5adaf84f30f
LeoLyo <7leolyo7@gmail.com>
2017-11-01 22:22:11 +0100

    Implementirano kretanje gore levo desno, dole nasumicno nekad crash-uje.

--- Digimoni/src/main/Main.java
+++ Digimoni/src/main/Main.java
@@ -9,8 +9,10 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		
-		GameFrame gf = new OpeningScreen("Digimoni", 1000, 800);
-		gf.initGameWindow();
+		/*GameFrame gf = new OpeningScreen("Digimoni", 1000, 800);
+		gf.initGameWindow();*/
+		GameFrame gFrame = new AnimatedSprite("SpriteSheets/GatomonSpriteSheetCombined.png");
+		gFrame.initGameWindow();
 	}
 
 	
